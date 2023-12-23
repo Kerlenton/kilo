f27df7be62817feb1967a593614252da6381949b
Kerlenton <semyonschv@yandex.ru>
2023-12-23 19:03:00 +0300

    Step 129

--- kilo.c
+++ kilo.c
@@ -362,7 +362,11 @@ void editorOpen(char *filename) {
 
 void editorSave() {
     if (E.filename == NULL) {
-        E.filename = editorPrompt("Save as: %s");
+        E.filename = editorPrompt("Save as: %s (ECS to cancel)");
+        if (E.filename == NULL) {
+            editorSetStatusMessage("Save aborted");
+            return;
+        }
     }
 
     int len;
